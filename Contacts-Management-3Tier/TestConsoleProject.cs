389acce54de9ccb6da88d6d41789f060e7199de1
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-25 00:17:57 +0300

    Add initial config, assembly info, and console entry point
    Added App.config for .NET Framework 4.8 support.
    Added AssemblyInfo.cs with standard metadata for Contacts_Test_Presentation.
    Added TestConsoleProject.cs with a basic Main method stub.

---
+++ Contacts-Management-3Tier/TestConsoleProject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Data;
+
+namespace Contacts_Test_Presentation
+{
+    internal class TestConsoleProject
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
