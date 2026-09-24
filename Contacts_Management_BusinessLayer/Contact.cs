781c5d23f4f3b23eebbe640280519768714e2009
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-25 00:18:13 +0300

    Add initial config and classes for business layer
    Added App.config targeting .NET Framework 4.8. Introduced Contact and Country classes in Contacts_Management_BusinessLayer namespace. Included AssemblyInfo.cs with standard metadata.

---
+++ Contacts_Management_BusinessLayer/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Management_BusinessLayer
+{
+    internal class Contact
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Contacts_Management_BusinessLayer/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts_Management_BusinessLayer
+{
+    internal class Country
+    {
+
+
+    }
+}
