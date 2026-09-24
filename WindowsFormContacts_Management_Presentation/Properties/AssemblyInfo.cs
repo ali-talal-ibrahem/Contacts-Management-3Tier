fa0aeeb808144815b9baebdc4eca0c46182b5edb
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-25 00:18:33 +0300

    Initial Windows Forms app setup and config files
    Set up WindowsFormContacts_Management_Presentation project targeting .NET Framework 4.8. Added App.config, Program.cs, MainForm, frmAdd_EditContact forms with designer and code-behind files. Included AssemblyInfo, resources, and settings files for application metadata and management.

---
+++ WindowsFormContacts_Management_Presentation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WindowsFormContacts_Management_Presentation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormContacts_Management_Presentation")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7e4be4cc-4a20-4c07-bb02-0c55bc242fee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WindowsFormContacts_Management_Presentation/frmAdd_EditContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormContacts_Management_Presentation
+{
+    public partial class frmAdd_EditContact : Form
+    {
+        public frmAdd_EditContact()
+        {
+            InitializeComponent();
+        }
+    }
+}
