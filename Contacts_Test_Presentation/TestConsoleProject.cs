9cb4a94319dd147c45544691e0ce0ee3d746e776
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-25 03:04:11 +0300

    Add test for finding contact by ID in TestConsoleProject
    Added testFindContactByID(int ID) to TestConsoleProject to find and display contact details by ID using clsContact.Find(ID). Updated Main to call this method with a sample ID (1).

--- Contacts_Test_Presentation/TestConsoleProject.cs
+++ Contacts_Test_Presentation/TestConsoleProject.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Linq;
 using System.Data;
+using Contacts_Management_BusinessLayer;
 
 namespace Contacts_Test_Presentation
 {
     internal class TestConsoleProject
     {
+
+        static void testFindContactByID(int ID)
+        { 
+            clsContact contact = clsContact.Find(ID);
+
+            if (contact == null)
+            {
+                Console.WriteLine("Contact {0} was not found.",ID);
+                return;
+            }
+
+            Console.WriteLine("ID: " + contact.ID);
+            Console.WriteLine("First Name: " + contact.FirstName);
+            Console.WriteLine("Last Name: " + contact.LastName);
+            Console.WriteLine("Email: " + contact.Email);
+            Console.WriteLine("Phone: " + contact.Phone);
+            Console.WriteLine("Address: " + contact.Address);
+            Console.WriteLine("Date Of Birth: " + contact.DateOfBirth);
+            Console.WriteLine("Country ID: " + contact.CountryID);
+            Console.WriteLine("Image Path: " + contact.ImagePath);
+        }
+        
+
+
         static void Main(string[] args)
         {
+            int contactID = 1;
 
-
+            testFindContactByID(contactID);
         }
     }
 }
