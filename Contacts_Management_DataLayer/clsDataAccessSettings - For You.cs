0fca0e7abd9bc50ca62ad96583e74172215f1efe
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-25 06:22:09 +0300

    Add Contacts_Management_DataLayer with data access class
    Added new namespace with static clsDataAccessSettings_ForYou class.
    Included warning comments for renaming and updating placeholders.
    Defined public static ConnectionString with placeholder values.

---
+++ Contacts_Management_DataLayer/clsDataAccessSettings - For You.cs
@@ -0,0 +1,19 @@
+﻿namespace Contacts_Management_DataLayer
+{
+
+#warning A very, very important note:
+
+    /*Before replacing your information, you must change the class name
+    from [ clsDataAccessSettings_ForYou ]  to [ clsDataAccessSettings ] so that the project works perfectly.*/
+
+    // FIX : Replace the phrase "[YOUR_SERVER_NAME]" with your actual server name.
+    // FIX : Replace the phrase "[YOUR_PASSWORD]" with your actual password.
+
+
+    static class clsDataAccessSettings_ForYou
+    //           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    {
+        static public string ConnectionString = "Server=.;Database=ContactsDB;User Id=[YOUR_SERVER_NAME];Password=[YOUR_PASSWORD]";
+        //                                                                            ^^^^^^^^^^^^^^^^^^          ^^^^^^^^^^^^^^^
+    }
+}
