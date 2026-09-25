17318aa09a8d4b20241697d3101b9091eee393ef
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-25 06:08:39 +0300

    Hiding the database configuration file
    This allows me to input my server details and password,
    while keeping the file in the repository, so you can safely enter your own server details and password.

--- Contacts_Management_DataLayer/clsDataAccessSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Contacts_Management_DataLayer
-{
-    static class clsDataAccessSettings
-    {
-        static public string ConnectionString = "Server=.;Database=ContactsDB;User Id=[YOUR_SERVER_NAME];Password=[YOUR_PASSWORD]";
-    }
-}
