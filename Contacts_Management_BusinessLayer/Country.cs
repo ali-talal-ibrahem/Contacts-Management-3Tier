6e80979cbb2088ef30a069ba087c07321095c833
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-26 07:58:31 +0300

    Refactor usings, add enum and properties for Country
    Cleaned up unused using directives and added Contacts_Management_DataLayer. Introduced private enMode enum for object state management. Added ID, CountryName, PhoneCode, and CountryCode properties. Implemented default and private constructors to initialize properties and set mode.

--- Contacts_Management_BusinessLayer/Country.cs
+++ Contacts_Management_BusinessLayer/Country.cs
@@ -1,14 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Contacts_Management_DataLayer;
 
 namespace Contacts_Management_BusinessLayer
 {
     public class clsCountry
     {
+        private enum enMode { eAddMode = 0 , eUpdateMode = 1};
+        private enMode Mode = enMode.eAddMode;
 
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string PhoneCode { get; set; }
+        public string CountryCode { get; set; }
+
+        public clsCountry() {
+
+            ID = -1;
+            CountryName = "";
+            PhoneCode = "";
+            CountryCode = "";
+            Mode = enMode.eAddMode;
+
+        }
+
+        private clsCountry(int ID,string CountryName, string PhoneCode,string CountryCode)
+        {
+
+            this.ID = ID;
+            this.CountryName = CountryName;
+            this.PhoneCode = PhoneCode;
+            this.CountryCode = CountryCode;
+            Mode = enMode.eUpdateMode;
+
+        }
 
     }
 }
