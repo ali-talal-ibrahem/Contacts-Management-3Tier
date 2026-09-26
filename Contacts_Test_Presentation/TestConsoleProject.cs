f27e7f4aa1c62b2eddeb485bd864abd58f1add9b
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-26 06:11:16 +0300

    Refactor contact DOB logic and update tests
    Set new contacts' DateOfBirth to a fixed value (2000-01-01 12:00:00) instead of DateTime.Now. Update contact editing to modify all fields and provide user feedback on update success or failure. Enable testGetAllContacts(); comment out testUpdateContactByID(109) and testUpdateContactByID(500).

--- Contacts_Test_Presentation/TestConsoleProject.cs
+++ Contacts_Test_Presentation/TestConsoleProject.cs
@@ -91,7 +91,7 @@ namespace Contacts_Test_Presentation
             NewContact.LastName = "LastName";
             NewContact.Email = "Email@example.com";
             NewContact.Phone = "+000000000";
-            NewContact.DateOfBirth = DateTime.Now;
+            NewContact.DateOfBirth = new DateTime(2000,01,01,12,00,00);
             NewContact.CountryID = 1;
             NewContact.Address = "Any Think";
             NewContact.ImagePath = "";
@@ -121,7 +121,22 @@ namespace Contacts_Test_Presentation
 
             if (Contact != null)
             {
-                Console.WriteLine($"\aContact ID {ID} Is Found !\n");
+                Contact.FirstName = "New-FirstName";
+                Contact.LastName = "New-LastName";
+                Contact.Email = "New-Email@example.com";
+                Contact.Phone = "New-+000000";
+                Contact.DateOfBirth = new DateTime(2001, 02, 02, 01, 00, 00);
+                Contact.CountryID = 1;
+                Contact.Address = "New-Address";
+                Contact.ImagePath = "";
+
+                if (Contact.Save())
+                {
+                    Console.WriteLine($"Contact information for the contact with ID [{ID}] has been successfully updated!");
+                }
+                else {
+                    Console.WriteLine("An error occurred while updating the contact details. Please try again.");
+                }
             }
             else {
                 Console.WriteLine($"\aContact ID {ID} Is Not Found !\n");
@@ -136,15 +151,15 @@ namespace Contacts_Test_Presentation
 
             //int contactID = 1;
             //testFindContactByID(contactID);
-            //testGetAllContacts();
+            testGetAllContacts();
             //testIsContactExist(6);
             //testDeleteContactByID(8);
             //testAddNewContact();
 
 
             // Test Function its Worked :
-            testUpdateContactByID(1);
-            testUpdateContactByID(500);
+            //testUpdateContactByID(109);
+            //testUpdateContactByID(500);
 
         }
     }
