7e08562bed9cc87fac45c92451b1afda3a0fc5fb
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-26 13:01:41 +0300

    Add GetAllCountries() to clsCountriesData for DB fetch
    Added a static GetAllCountries() method to clsCountriesData to retrieve all records from the "Countries" table using SqlConnection and SqlCommand. Results are loaded into a DataTable and returned. Includes basic exception handling and ensures the connection is closed properly.

--- Contacts_Management_DataLayer/CountriesData.cs
+++ Contacts_Management_DataLayer/CountriesData.cs
@@ -7,6 +7,40 @@ namespace Contacts_Management_DataLayer
     public class clsCountriesData
     {
 
+        public static DataTable GetAllCountries() {
+
+            DataTable dt = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Countries";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) {
+                    dt.Load(reader);
+                }
+
+                reader.Close();
+            }
+            catch
+            {
+
+            }
+            finally {
+                connection.Close();
+            }
+
+            return dt;
+        }
+
+
 
     }
 }
