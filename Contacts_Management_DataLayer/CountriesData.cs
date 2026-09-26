62383934ef6e46189969bf5ecd77934ecc4e86d3
Ali Ibrahem <aliibrahem760work@gmail.com>
2026-09-26 07:56:59 +0300

    Clean up and update namespace imports
    Removed unused imports (Generic, Linq, Text, Tasks).
    Added System.Data and System.Data.SqlClient for DB support.

--- Contacts_Management_DataLayer/CountriesData.cs
+++ Contacts_Management_DataLayer/CountriesData.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Contacts_Management_DataLayer
 {
